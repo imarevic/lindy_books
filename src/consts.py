ee5e2e631d0d4fe3a26fefe8e46ab132ec526a37
imarevic <i.marevic@gmx.de>
2020-08-30 23:43:03 +0200

    response parsing

--- src/consts.py
+++ src/consts.py
@@ -1,5 +1,9 @@
 base_url = 'https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json'
+api_key_name = 'api-key'
+# offsets
 initial_offset = 0
 op_offset = 20
-api_key_name = 'api-key'
-req_sleep = 5
+# api limits
+max_req_per_minute = 10
+max_req_per_day = 4000
+req_sleep = 60 / max_req_per_minute
--- src/logic.py
+++ src/logic.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import math
+import time
 from src.helpers import *
 import src.consts as c
 
@@ -16,7 +17,7 @@ def get_ny_times_data(auth_filename):
     else:
         n_iter = math.ceil(n_results / c.op_offset)
     # get base schema
-    
+
 
     # load data
     data = get_books_data(n_iter, c.req_sleep, api_key)
@@ -24,10 +25,42 @@ def get_ny_times_data(auth_filename):
 def get_books_data(n_iter, t_sleep, api_token):
 
     i_offset = c.op_offset
-    for batch in range(1, 4):
+    for batch in range(1, 2):
          resp = get_data(i_offset, api_token)
-         data_batch = pd.json_normalize(resp['results'])
+         resp_list = resp['results']
+         resp_dict = process_response(resp_list)
 
+        # sleep for 6 seconds so we do not exceed limit
+         time.sleep(t_sleep)
          i_offset+=c.op_offset
 
+    return data_batch
+
+def process_response(resp_list):
+
+    # initialize empty df
+    final_df = pd.DataFrame(columns=['title', 'description', 'contributor', 'author', 'contributor_note',
+                                     'price', 'age_group', 'publisher', 'rank', 'list_name', 'display_name',
+                                     'published_date', 'bestsellers_date', 'weeks_on_list', 'ranks_last_wek',
+                                     'asterix', 'dagger'])
+    # process data and append to df
+    for elem in resp_list:
+        f_dict = {key : [value] for key, value in elem.items()}
+        f_dict['rank'] = [d['rank'] for d in f_dict['ranks_history'][0]]
+        f_dict['list_name'] = [d['list_name'] for d in f_dict['ranks_history'][0]]
+        f_dict['display_name'] = [d['display_name'] for d in f_dict['ranks_history'][0]]
+        f_dict['published_date'] = [d['published_date'] for d in f_dict['ranks_history'][0]]
+        f_dict['bestsellers_date'] = [d['bestsellers_date'] for d in f_dict['ranks_history'][0]]
+        f_dict['weeks_on_list'] = [d['weeks_on_list'] for d in f_dict['ranks_history'][0]]
+        f_dict['ranks_last_week'] = [d['ranks_last_week'] for d in f_dict['ranks_history'][0]]
+        f_dict['asterisk'] = [d['asterisk'] for d in f_dict['ranks_history'][0]]
+        f_dict['dagger'] = [d['dagger'] for d in f_dict['ranks_history'][0]]
+        # cleanup
+        del f_dict['isbns']
+        del f_dict['ranks_history']
+        del f_dict['reviews']
+        # convert to df and full up key values
+        f_df = pd.DataFrame.from_dict(f_dict, orient='index').transpose()
+        final_df = pd.concat([final_df, f_df])
+        print(final_df)
     return None
