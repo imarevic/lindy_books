1351431ac67f59f49eac9051f47b91e8ffd0318c
imarevic <i.marevic@gmx.de>
2020-08-31 14:47:49 +0200

    notebook

--- README.md
+++ README.md
@@ -7,11 +7,32 @@ The notebook in this repo extracts books from the NY Times Bestseller list that 
 > The Lindy effect is a theory that the future life expectancy of some non-perishable things like a technology, an idea
 > or a book is proportional to their current age, so that every additional period of survival implies a longer remaining life expectancy.
 
+*Dependencies:* `Python 3`
+
 ---
 **Setup**
 
+1. run `pip install requirements.txt` in the console (ideally in a virtual environment).
+2. create a developer account at https://developer.nytimes.com/ and create an app. This will provide
+   you with an API-key.
+3. create an `auth.yaml` file in the root directory of this project and copy your NY-Times API-KEY into
+   the file like so:
+
+    `api-key : 'YOUR API KEY'`
+
 ---
 **Usage**
+
+1. run `jupyter notebook` on the console and open `lindy_books_list.ipynb`.
+2. if you want to load just a few batches of data (each batch contains 20 books) then
+   set `n_batches` in the function call `get_ny_times_data('auth.yaml', n_batches=None)`.
+   If nothing es set, then all the data is loaded.
+3. Click on `Cell` and `Run All`.
+4. Sit back and wait for a few hours until results are displayed.
+5. Modify the notebook to explore books regarding different dimensions.
+
+Note: Fetching the data from the NY-Times-API takes approx. 2-3 hours due to request limits.
+If you want to speed up things contact NY-Times and request to lift these limits.
 
 ---
 **License**<br>
--- src/logic.py
+++ src/logic.py
@@ -5,7 +5,14 @@ import time
 from src.helpers import *
 import src.consts as c
 
-def get_ny_times_data(auth_filename):
+# initialize empty df
+init_df = pd.DataFrame(columns=['title', 'description', 'contributor', 'author', 'contributor_note',
+                                     'price', 'age_group', 'publisher', 'rank', 'list_name', 'display_name',
+                                     'published_date', 'bestsellers_date', 'weeks_on_list', 'ranks_last_week',
+                                     'asterix', 'dagger'])
+
+
+def get_ny_times_data(auth_filename, n_batches=None):
 
     # api key
     api_key = get_api_key('auth.yaml')
@@ -16,33 +23,34 @@ def get_ny_times_data(auth_filename):
         n_iter = n_results / c.op_offset
     else:
         n_iter = math.ceil(n_results / c.op_offset)
-    # get base schema
+    if n_batches != None:
+        n_iter = n_batches
+    # estimate loading time
+    estimated_loading_time = (n_iter * c.req_sleep) // 60
+    print("Estimated loading time: {} minutes.".format(estimated_loading_time))
+    # load and preprocess data
+    data = get_books_data(init_df, n_iter, c.req_sleep, api_key)
+    return data
 
+def get_books_data(init_df, n_iter, t_sleep, api_token):
 
-    # load data
-    data = get_books_data(n_iter, c.req_sleep, api_key)
-
-def get_books_data(n_iter, t_sleep, api_token):
-
+    data_batch = init_df.copy()
     i_offset = c.op_offset
-    for batch in range(1, 2):
-         resp = get_data(i_offset, api_token)
-         resp_list = resp['results']
-         resp_dict = process_response(resp_list)
-
+    for batch in range(1, n_iter):
+        resp = get_data(i_offset, api_token)
+        resp_list = resp['results']
+        resp_df = process_response(init_df, resp_list)
+        data_batch = pd.concat([data_batch, resp_df])
         # sleep for 6 seconds so we do not exceed limit
-         time.sleep(t_sleep)
-         i_offset+=c.op_offset
+        print("{}/{} batches loaded.".format(batch, n_iter))
+        time.sleep(t_sleep)
+        i_offset+=c.op_offset
 
     return data_batch
 
-def process_response(resp_list):
+def process_response(init_df, resp_list):
 
-    # initialize empty df
-    final_df = pd.DataFrame(columns=['title', 'description', 'contributor', 'author', 'contributor_note',
-                                     'price', 'age_group', 'publisher', 'rank', 'list_name', 'display_name',
-                                     'published_date', 'bestsellers_date', 'weeks_on_list', 'ranks_last_wek',
-                                     'asterix', 'dagger'])
+    processed_df = init_df.copy()
     # process data and append to df
     for elem in resp_list:
         f_dict = {key : [value] for key, value in elem.items()}
@@ -59,8 +67,9 @@ def process_response(resp_list):
         del f_dict['isbns']
         del f_dict['ranks_history']
         del f_dict['reviews']
-        # convert to df and full up key values
+        # convert to df and fill up key values
         f_df = pd.DataFrame.from_dict(f_dict, orient='index').transpose()
-        final_df = pd.concat([final_df, f_df])
-        print(final_df)
-    return None
+        f_df.fillna(method='ffill', inplace=True)
+        processed_df = pd.concat([processed_df, f_df])
+
+    return processed_df
